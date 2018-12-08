fad7af95a16546497520ccf58aa4b76cb71a6bb5
Kyle Andrews <kylea@page.works>
2018-12-08 10:02:10 -0500

    Fixes dotenv vars for front-end use

--- app/scripts/env.ts
+++ app/scripts/env.ts
@@ -1,11 +1,9 @@
-import dotenv from 'dotenv';
-const env = dotenv.config({ path: '../../.env' });
+const APP_NAME          = 'REPLACE_ME';
+const isDebug           = true;
 
-const APP_NAME          = process.env.PROJECT_NAME.replace(/\s/g, '-').toLowerCase();
 const html              = document.documentElement;
 const body              = document.body;
 const pjaxContainer     = '.js-pjax-container';
 const pjaxWrapper       = '.js-pjax-wrapper';
-const isDebug           = (process.env.ENVIRONMENT === 'dev') ? true : false;
 
 export { APP_NAME, html, body, pjaxWrapper, pjaxContainer, isDebug };
