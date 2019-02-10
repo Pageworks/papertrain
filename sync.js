e095b1dc7c294aa883fc95240203bf617db79c6a
Kyle Andrews <andrews@gamesbykyle.com>
2019-02-10 14:57:55 -0500

    Updates browser-sync script

--- sync.js
+++ sync.js
@@ -17,15 +17,15 @@ browserSync.watch("./public/assets").on("change", browserSync.reload);
 
 browserSync.watch("./app/typescript").on("change", ()=>{
     const message = 'Compiling TypeScript';
-    browserSync.notify(`<div style="position:fixed;top:0;left:0;display:inline-flex;width:100vw;height:100vh;justify-content:center;align-items:center;background-color:#fafafa"><span style="display:inline-block;padding:16px 32px;border-radius:4px;background-color:#ffffff;box-shadow: 0 4px 32px rgba(75,78,109,0.1);font-size:22px;color:#414141;">${message}</span></div>`, Infinity);
+    browserSync.notify(`<div style="position:fixed;top:0;left:0;display:inline-flex;width:100vw;height:100vh;justify-content:center;align-items:center;background-color:rgba(0,0,0,0.6);"><span style="display:inline-block;padding:16px 32px;border-radius:4px;background-color:#ffffff;box-shadow: 0 2px 8px rgba(75,78,109,0.87);font-size:22px;color:#3b83f9;">${message}</span></div>`, Infinity);
 });
 
 browserSync.watch("./_compiled").on("change", ()=>{
     const message = 'Bundling Modules';
-    browserSync.notify(`<div style="position:fixed;top:0;left:0;display:inline-flex;width:100vw;height:100vh;justify-content:center;align-items:center;background-color:#fafafa"><span style="display:inline-block;padding:16px 32px;border-radius:4px;background-color:#ffffff;box-shadow: 0 4px 32px rgba(75,78,109,0.1);font-size:22px;color:#414141;">${message}</span></div>`, Infinity);
+    browserSync.notify(`<div style="position:fixed;top:0;left:0;display:inline-flex;width:100vw;height:100vh;justify-content:center;align-items:center;background-color:rgba(0,0,0,0.6);"><span style="display:inline-block;padding:16px 32px;border-radius:4px;background-color:#ffffff;box-shadow: 0 2px 8px rgba(75,78,109,0.87);font-size:22px;color:#3b83f9;">${message}</span></div>`, Infinity);
 });
 
 browserSync.watch("./app/sass").on("change", ()=>{
     const message = 'Compiling SCSS';
-    browserSync.notify(`<div style="position:fixed;top:0;left:0;display:inline-flex;width:100vw;height:100vh;justify-content:center;align-items:center;background-color:#fafafa"><span style="display:inline-block;padding:16px 32px;border-radius:4px;background-color:#ffffff;box-shadow: 0 4px 32px rgba(75,78,109,0.1);font-size:22px;color:#414141;">${message}</span></div>`, Infinity);
+    browserSync.notify(`<div style="position:fixed;top:0;left:0;display:inline-flex;width:100vw;height:100vh;justify-content:center;align-items:center;background-color:rgba(0,0,0,0.6);"><span style="display:inline-block;padding:16px 32px;border-radius:4px;background-color:#ffffff;box-shadow: 0 2px 8px rgba(75,78,109,0.87);font-size:22px;color:#3b83f9;">${message}</span></div>`, Infinity);
 });
