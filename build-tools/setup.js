64c1c20e9d24687e80d35dd440c334f04a226c92
Kyle Andrews <kylea@page.works>
2019-04-29 11:26:24 -0400

    Fixes setup console log bug

--- build-tools/setup.js
+++ build-tools/setup.js
@@ -13,6 +13,7 @@ figlet.text('Papertrain', {
 }, (err, data)=>{
     console.log(chalk.cyan(data));
     console.log(chalk.cyan('----------------------------------------'));
+    askQuestions();
 });
 
 askQuestions = (async ()=>{
@@ -86,83 +87,84 @@ askQuestions = (async ()=>{
 
                     envGenSpinner.text = 'Enviroment file successfully created';
                     envGenSpinner.succeed();
+                    cleanupFiles();
                 });
             });
         });
     }
 });
 
-const readme = ora();
-readme.spinner = 'dots';
-readme.text = 'Removing readme file';
-readme.start();
-if(fs.existsSync('./README.md')){
-    fs.unlink('./README.md', (err)=>{
-        if(err){
-            readme.text = 'Failed to remove the readme file';
-            readme.fail();
-        }
-        readme.text = 'Readme has been removed';
-        readme.succeed();
-    });
-}else{
-    readme.stop();
-}
-
-const changelog = ora();
-changelog.spinner = 'dots';
-changelog.text = 'Removing changelog file';
-changelog.start();
-if(fs.existsSync('./CHANGELOG.md')){
-    fs.unlink('./CHANGELOG.md', (err)=>{
-        if(err){
-            changelog.text = 'Failed to remove the changelog file';
-            changelog.fail();
-        }
-        changelog.text = 'Changelog has been removed';
-        changelog.succeed();
-    });
-}else{
-    changelog.stop();
-}
-
-const license = ora();
-license.spinner = 'dots';
-license.text = 'Removing license file';
-license.start();
-if(fs.existsSync('./LICENSE')){
-    fs.unlink('./LICENSE', (err)=>{
-        if(err){
-            license.text = 'Failed to remove the license file';
-            license.fail();
-        }
-        license.text = 'License has been removed';
-        license.succeed();
-    });
-}else{
-    license.stop();
-}
-
-const gitignore = ora();
-gitignore.spinner = 'dots';
-gitignore.text = 'Removing license file';
-gitignore.start();
-if(fs.existsSync('./.gitignore')){
-    fs.readFile('./.gitignore', 'utf-8', (err, file)=>{
-        if(err){
-            gitignore.text = 'Failed to open the gitignore file';
-            gitignore.fail();
-            throw err;
-        }
-
-        var newValue = file.replace(/vendor/g, '');
-        fs.writeFile('./.gitignore', newValue, 'utf-8', (err)=>{
-            gitignore.text = 'Vendors directory has been removed from the gitignore file';
-            gitignore.succeed();
+function cleanupFiles(){
+    const readme = ora();
+    readme.spinner = 'dots';
+    readme.text = 'Removing readme file';
+    readme.start();
+    if(fs.existsSync('./README.md')){
+        fs.unlink('./README.md', (err)=>{
+            if(err){
+                readme.text = 'Failed to remove the readme file';
+                readme.fail();
+            }
+            readme.text = 'Readme has been removed';
+            readme.succeed();
         });
-    });
-}else{
-    gitignore.stop();
-}
+    }else{
+        readme.stop();
+    }
 
-askQuestions();
+    const changelog = ora();
+    changelog.spinner = 'dots';
+    changelog.text = 'Removing changelog file';
+    changelog.start();
+    if(fs.existsSync('./CHANGELOG.md')){
+        fs.unlink('./CHANGELOG.md', (err)=>{
+            if(err){
+                changelog.text = 'Failed to remove the changelog file';
+                changelog.fail();
+            }
+            changelog.text = 'Changelog has been removed';
+            changelog.succeed();
+        });
+    }else{
+        changelog.stop();
+    }
+
+    const license = ora();
+    license.spinner = 'dots';
+    license.text = 'Removing license file';
+    license.start();
+    if(fs.existsSync('./LICENSE')){
+        fs.unlink('./LICENSE', (err)=>{
+            if(err){
+                license.text = 'Failed to remove the license file';
+                license.fail();
+            }
+            license.text = 'License has been removed';
+            license.succeed();
+        });
+    }else{
+        license.stop();
+    }
+
+    const gitignore = ora();
+    gitignore.spinner = 'dots';
+    gitignore.text = 'Removing license file';
+    gitignore.start();
+    if(fs.existsSync('./.gitignore')){
+        fs.readFile('./.gitignore', 'utf-8', (err, file)=>{
+            if(err){
+                gitignore.text = 'Failed to open the gitignore file';
+                gitignore.fail();
+                throw err;
+            }
+
+            var newValue = file.replace(/vendor/g, '');
+            fs.writeFile('./.gitignore', newValue, 'utf-8', (err)=>{
+                gitignore.text = 'Vendors directory has been removed from the gitignore file';
+                gitignore.succeed();
+            });
+        });
+    }else{
+        gitignore.stop();
+    }
+}
