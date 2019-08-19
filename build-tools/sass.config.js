13d9fc7aba203277d4292f1156d0404cae72069f
Kyle Andrews <kylea@page.works>
2019-08-19 18:57:42 -0400

    Reworked sass compiler script

--- build-tools/sass.config.js
+++ build-tools/sass.config.js
@@ -4,92 +4,158 @@ const fs = require('fs');
 const rimraf = require("rimraf");
 const chalk = require('chalk');
 
-// Create a timestamp for cache busting
-const timestamp = Date.now().toString();
-
-// Get all the base SCSS files from the base global scss directory
-const globalFiles = glob.sync('./sass/*.scss');
-
-// Get all the SCSS files from the templates lib directory
-const templateFiles = glob.sync('./templates/**/*.scss');
-
-// Concat the arrays
-const files = [...globalFiles, ...templateFiles];
-
-let success = true;
-
-compileSASS();
-
-function compileSASS(){
-    console.log(chalk.white('Compiling SASS'));
-
-    // Make the new CSS directory
-    fs.mkdirSync(`./public/automation/styles-${ timestamp }`);
-
-    // Generate the files
-    for(let i = 0; i < files.length; i++){
-        const file = files[i];
-        sass.render(
-            {
-                file: file,
-                outputStyle: 'compressed',
-                includePaths: ['sass/settings', 'sass/tools']
-            },
-            function(error, result){
-                if (error) {
-                    console.log(chalk.hex('#f57b7b').bold(`SASS Compile Error:`), chalk.white(`${ error.message } at line`), chalk.yellow.bold(error.line), chalk.hex('#ffffff').bold(error.file));
-                }else{
-                    const fileName = result.stats.entry.match(/[ \w-]+?(?=\.)/gi)[0].toLowerCase();
-                    if(fileName){
-                        const newFile = `./public/automation/styles-${ timestamp }/` + fileName + '.css';
-                        console.log(chalk.hex('#ffffff').bold(file), chalk.hex('#8cf57b').bold(' [compiled]'));
-                        cleanup(files.indexOf(file));
-                        fs.writeFile(newFile, result.css.toString(), function (err) {
-                            if(err){
-                                success = false;
-                            };
-                        });
-                    }else{
-                        console.log('Something went wrong with the file name of ' + result.stats.entry);
-                        success = false;
-                    }
-                }
-            }
-        );
-    }
-}
-
-function cleanup(index){
-    if(index !== files.length - 1){
-        return;
+class SassCompiler
+{
+    constructor()
+    {
+        console.log(chalk.white('Compiling SASS'));
+        this.run();
     }
 
-    if(!success){
-        console.log(chalk.white('Something went wrong, the previous CSS will not be removed'));
-        return;
-    }
-
-    console.log(chalk.white('Removing stale CSS'));
-
-    const path = `public/automation`;
-    const allDirs = fs.readdirSync(path);
-    const styleDirs = [];
-    for(let i = 0; i < allDirs.length; i++){
-        if(allDirs[i].match(/styles-.*/)){
-            styleDirs.push(allDirs[i]);
+    async run()
+    {
+        try
+        {
+            const globalFiles = await this.getFiles('./sass/*.scss');
+            const templateFiles = await this.getFiles('./templates/**/*.scss');
+            const files = [...globalFiles, ...templateFiles];
+            const timestamp = await this.getTimestamp();
+            await this.makeDirectory(timestamp);
+            await this.compile(files, timestamp);
+            await this.cleanup(timestamp);
+        }
+        catch (error)
+        {
+            throw error;
         }
     }
 
-    for(i = 0; i < styleDirs.length; i++){
-        const directoryTimestamp = styleDirs[i].match(/[^styles-].*/)[0];
-
-        if(parseInt(directoryTimestamp) < parseInt(timestamp)){
-            rimraf(`public/automation/${ styleDirs[i] }`, (err)=>{
-                if(err){
-                    console.log(`Failed to remove ${ styleDirs[i] }`);
-                    throw err;
+    getFiles(path)
+    {
+        return new Promise((resolve, reject)=>{
+            glob(path, (error, files)=>{
+                if (error)
+                {
+                    reject(error);
                 }
+
+                resolve(files);
             });
+        });
+    }
+
+    getTimestamp()
+    {
+        return new Promise((resolve, reject)=>{
+            fs.readFile('config/papertrain/automation.php', (error, buffer)=>{
+                if (error)
+                {
+                    reject(error);
+                }
+
+                const data = buffer.toString();
+                const timestamp = data.match(/\d+/g)[0];
+                resolve(timestamp);
+            });
+        });
+    }
+
+    makeDirectory(timestamp)
+    {
+        return new Promise((resolve, reject)=>{
+            fs.mkdir(`./public/automation/styles-${ timestamp }`, (error)=>{
+                if (error)
+                {
+                    reject(error);
+                }
+
+                resolve();
+            });
+        });
+    }
+
+    compile(files, timestamp)
+    {
+        return new Promise((resolve, reject)=>{
+            let count = 0;
+
+            for (let i = 0; i < files.length; i++)
+            {
+                const file = files[i];
+                sass.render(
+                    {
+                        file: file,
+                        outputStyle: 'compressed',
+                        includePaths: ['sass/settings', 'sass/tools']
+                    },
+                    (error, result)=>{
+                        if (error)
+                        {
+                            reject(`${ error.message } at line ${ error.line } ${ error.file }`);
+                        }
+                        else
+                        {
+                            const fileName = result.stats.entry.match(/[\w-]+?(?=\.)/gi)[0].toLowerCase();
+                            if (fileName)
+                            {
+                                const newFile = `./public/automation/styles-${ timestamp }/${ fileName }.css`;
+                                fs.writeFile(newFile, result.css.toString(), (error)=>{
+                                    if (error)
+                                    {
+                                        reject('Something went wrong saving the file' + error);
+                                    }
+
+                                    console.log(chalk.hex('#ffffff').bold(file), chalk.hex('#8cf57b').bold(' [compiled]'));
+                                    count++;
+
+                                    if (count === files.length)
+                                    {
+                                        resolve();
+                                    }
+                                });
+                            }
+                            else
+                            {
+                                reject('Something went wrong with the file name of ' + result.stats.entry);
+                            }
+                        }
+                    }
+                );
+            }
+        });
+    }
+
+    cleanup(timestamp)
+    {
+        console.log(chalk.white('Removing stale CSS'));
+
+        const path = `public/automation`;
+        const allDirs = fs.readdirSync(path);
+        const styleDirs = [];
+        for (let i = 0; i < allDirs.length; i++)
+        {
+            if (allDirs[i].match(/styles-.*/))
+            {
+                styleDirs.push(allDirs[i]);
+            }
+        }
+
+        for (let i = 0; i < styleDirs.length; i++)
+        {
+            const directoryTimestamp = styleDirs[i].match(/[^styles-].*/)[0];
+
+            if (parseInt(directoryTimestamp) < parseInt(timestamp))
+            {
+                rimraf(`public/automation/${ styleDirs[i] }`, (err)=>{
+                    if (err)
+                    {
+                        console.log(`Failed to remove ${ styleDirs[i] }`);
+                        throw err;
+                    }
+                });
+            }
         }
     }
 }
+
+new SassCompiler();
