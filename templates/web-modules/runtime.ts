c845d72d4ba9581c6697ae104fabf2f3dea361fb
Kyle Andrews <kylea@page.works>
2019-11-02 10:24:08 -0400

    Added logic for observing and loading web components

--- templates/web-modules/runtime.ts
+++ templates/web-modules/runtime.ts
@@ -10,6 +10,7 @@ interface WorkerResponse
 class Runtime
 {
     private _bodyParserWorker : Worker;
+    private _io : IntersectionObserver;
 
     constructor()
     {
@@ -18,8 +19,38 @@ class Runtime
             this._bodyParserWorker.postMessage(document.body.innerHTML);
         });
         this._bodyParserWorker.onmessage = this.handleWorkerMessage.bind(this);
-        
+        this._io = new IntersectionObserver(this.intersectionCallback);
         new DeviceManager(env.isDebug, true);
+    }
+
+    private intersectionCallback:IntersectionObserverCallback = this.handleIntersection.bind(this);
+
+    private handleIntersection(entries:Array<IntersectionObserverEntry>)
+    {
+        for (let i = 0; i < entries.length; i++)
+        {
+            if (entries[i].isIntersecting)
+            {
+                this._io.unobserve(entries[i].target);
+                entries[i].target.setAttribute('state', 'loading');
+                const customElement = entries[i].target.tagName.toLowerCase().trim();
+                let el = document.head.querySelector(`script[file="${ customElement }.js"]`);
+                if (!el)
+                {
+                    el = document.createElement('script');
+                    el.setAttribute('file', `${ customElement }.js`);
+                    document.head.append(el);
+                    el.addEventListener('load', () => {
+                        entries[i].target.setAttribute('state', 'loaded');
+                    });
+                    this.fetchFile(el, customElement, 'js');
+                }
+                else
+                {
+                    entries[i].target.setAttribute('state', 'loaded');
+                }
+            }
+        }
     }
 
     private handleWorkerMessage(e:MessageEvent)
@@ -29,6 +60,7 @@ class Runtime
         {
             this.fetchResources(response.files, 'link', 'css').then(() => {
                 document.documentElement.setAttribute('state', 'idling');
+                this.handleWebComponents();
             });
         }
         else if (response.type === 'lazy')
@@ -40,7 +72,7 @@ class Runtime
         }
     }
 
-    private async fetchFile(element:Element, filename:string, filetype:string)
+    private async fetchFile(element:Element, filename:string, filetype:'css'|'js')
     {
         const url = `${ window.location.origin }/automation/${ filename }.${ filetype }`;
         switch (filetype)
@@ -93,6 +125,16 @@ class Runtime
                 }
             }
         });
+    }
+
+    private handleWebComponents() : void
+    {
+        const customElements = Array.from(document.body.querySelectorAll('[web-component]:not([state])'));
+        for (let i = 0; i < customElements.length; i++)
+        {
+            customElements[i].setAttribute('state', 'unseen');
+            this._io.observe(customElements[i]);
+        }
     }
 }
 new Runtime();
